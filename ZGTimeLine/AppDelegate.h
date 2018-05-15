f555f7a22ee71ae6e1a525abd6d250d6f20c8f7d
zhangzhi0903 <18310127735@163.com>
2018-05-15 09:27:50 +0800

    Initial Commit

---
+++ ZGTimeLine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGTimeLine
+//
+//  Created by offcn_zcz32036 on 2018/5/15.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZGTimeLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZGTimeLine
+//
+//  Created by offcn_zcz32036 on 2018/5/15.
+//  Copyright © 2018年 cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
